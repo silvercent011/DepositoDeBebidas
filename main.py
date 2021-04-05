5402338187a71079732c24b5f96d9728e67b82a0
Sidney Alex <silvercenturion2@gmail.com>
2021-04-05 17:16:26 -0300

    first templates

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from env import DB_INFO
+# from env import DB_INFO
 import json
+
+with open('./env.json','r') as env:
+    DB_INFO = json.loads(env.read())
 
 #Routes
 from routes.users import *
@@ -21,14 +24,19 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
 class Funcionarios(Resource):
     def get(self):
         funcionarios = [funcionario for funcionario in db_session.execute("SELECT * FROM funcionarios")]
-        return {'funcionarios': str(funcionarios)}
+        return {'funcionarios': funcionarios}
+@app.route('/funcionarios')
+def funcionarios():
+    funcionarios = Funcionarios().get()
+    return render_template('funcionarios.html', funcionarios=funcionarios['funcionarios'])
 
-api.add_resource(Auth, '/auth')
-api.add_resource(HelloWorld, '/')
-api.add_resource(Funcionarios, '/funcionarios')
+# api.add_resource(Auth, '/auth')
+# api.add_resource(HelloWorld, '/')
+# api.add_resource(Funcionarios, '/funcionarios')
 
 if __name__ == '__main__':
     app.run(debug=True)
